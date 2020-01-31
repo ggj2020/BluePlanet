66f751dffafe5db58b1a8c98e0a01f4757f70232
cody.game <cody.game@kakaocorp.com>
2020-02-01 03:48:25 +0900

    동일한 퍼즐블럭 컨택 검출 및 삭제 로직 구현

---
+++ Assets/Scripts/Puzzle/PuzzleElement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections;
+
+public class PuzzleElement : MonoBehaviour
+{
+    static private float contactReDetectSeconds = 2f;
+    void OnCollisionStay2D(Collision2D collision2d)
+    {
+        if ( gameObject.name == collision2d.gameObject.name )
+        {
+            StartCoroutine(DetectContactAfterFewSeconds(collision2d.collider, collision2d.otherCollider));
+        }
+    }
+
+    private IEnumerator DetectContactAfterFewSeconds(Collider2D collider, Collider2D otherCollider)
+    {
+        yield return new WaitForSeconds(contactReDetectSeconds);
+        if ( collider != null && otherCollider != null && collider.Distance(otherCollider).distance <= 0 )
+        {
+            Destroy(collider.gameObject);
+            Destroy(otherCollider.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Puzzle/PuzzleElementGenerator.cs
+++ Assets/Scripts/Puzzle/PuzzleElementGenerator.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class ElementGenerator : MonoBehaviour
+public class PuzzleElementGenerator : MonoBehaviour
 {
 
     private string[] elements = new string[] {
