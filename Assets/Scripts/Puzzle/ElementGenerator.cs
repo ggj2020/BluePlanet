959c420586b291543fd582db407273142556df39
cody.game <cody.game@kakaocorp.com>
2020-02-01 02:56:55 +0900

    퍼즐 블럭 생성

---
+++ Assets/Scripts/Puzzle/ElementGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementGenerator : MonoBehaviour
+{
+
+    private string[] elements = new string[] {
+        "Puzzle/Prefabs/Pentagon",
+        "Puzzle/Prefabs/RoundedRectangle",
+        "Puzzle/Prefabs/Triangle"
+    };
+
+    void Update()
+    {
+        if( Input.GetKeyDown(KeyCode.Space) )
+        {
+            this.Generate();
+        }
+    }
+
+    void Generate()
+    {
+        GameObject prefab = Resources.Load<GameObject>(elements.Pick());
+        Instantiate<GameObject>(prefab, transform);
+    }
+}
---
+++ Assets/Scripts/Puzzle/Utility.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public static class Utility
+{
+    public static T Pick<T>(this ICollection<T> collection) where T : class {
+        if ( collection.Count == 0 )
+        {
+            return null;
+        }
+        else
+        {
+            return collection.ElementAt(Random.Range(0, collection.Count));
+        }
+    }
+}
