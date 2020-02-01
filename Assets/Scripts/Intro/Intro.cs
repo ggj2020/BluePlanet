c24c017c84b279d3214bf28c9d39ba6a163c8e2b
cody.game <cody.game@kakaocorp.com>
2020-02-01 15:57:46 +0900

    인트로 구현을 위한 리소스 추가

---
+++ Assets/Scripts/Intro/Intro.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour
+{
+    private Queue<Cut> cuts = new Queue<Cut>();
+    void Start()
+    {
+    }
+}
+
+public struct Cut {
+}
