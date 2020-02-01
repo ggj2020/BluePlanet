f83b4d86ce261c9de96aadd5951022997c42f484
kojdj0811 <kojdj0811@gmail.com>
2020-02-02 02:40:06 +0900

    "00_TitleSceneGraphic" scene update.

---
+++ Assets/_jdj/_Scripts/CsRotatePlanet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CsRotatePlanet : MonoBehaviour
+{
+    public Vector3 rotationSpeed;
+
+
+    void Update()
+    {
+        transform.localEulerAngles += rotationSpeed * Time.deltaTime;
+    }
+}
