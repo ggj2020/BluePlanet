837664940bb63ef1c8531eefc972d4fbb784c2cd
kojdj0811 <kojdj0811@gmail.com>
2020-02-02 11:22:29 +0900

    타이틀 버튼 이미지 수정 이펙트 메니저 추가

---
+++ Assets/_jdj/_Scripts/CsDestroySelf.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CsDestroySelf : MonoBehaviour
+{
+    public float life;
+
+    void Start()
+    {
+        Destroy(gameObject, life);
+    }
+}
