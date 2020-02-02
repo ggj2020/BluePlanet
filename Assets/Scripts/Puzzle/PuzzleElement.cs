b1ef10744028d9535899dd2767e0ede07451465a
cody.game <cody.game@kakaocorp.com>
2020-02-02 11:52:02 +0900

    연쇄 삭제를 위한 임시 커밋

--- Assets/Scripts/Puzzle/PuzzleElement.cs
+++ Assets/Scripts/Puzzle/PuzzleElement.cs
@@ -1,37 +1,43 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine.EventSystems;
+
+public interface PuzzleElementRemover
+{
+    void RegisterDeleteCandidate(PuzzleElement contacted);
+    void UnregisterDeleteCandidate(PuzzleElement contacted);
+    void RemoveCandidates();
+}
 
 [RequireComponent(typeof(Collider2D))]
 [RequireComponent(typeof(SpriteRenderer))]
 public class PuzzleElement : MonoBehaviour, IPointerClickHandler
 {
-    private GameObject _contactedGameObject;
-    private GameObject contactedGameObject
-    {
-        get
-        {
-            return this._contactedGameObject;
-        }
-        set 
-        {
-            var material = GetComponent<SpriteRenderer>().material;
-            material.SetFloat("_OutlineBrightness", value == null ? 0f : 2.8f);
-            material.SetFloat("_OutlineWidth", value == null ? 0f : 0.0156f);
-            this._contactedGameObject = value;
-        }
-    }
+    public PuzzleElementRemover remover = null;
+    // private List<PuzzleElement> contactedGameObjects = new List<PuzzleElement>();
 
+    private SpriteRenderer spriteRenderer = null;
     void Start()
     {
-        this.contactedGameObject = null;
+        this.spriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // this.spriteRenderer.color = this.contactedGameObjects.Count > 0 ? Color.green : Color.white;
     }
 
     void OnCollisionStay2D(Collision2D collision2d)
     {
         if ( gameObject.name == collision2d.gameObject.name )
         {
-            this.contactedGameObject = collision2d.collider.gameObject;
+            // var puzzleElement = collision2d.collider.gameObject.GetComponent<PuzzleElement>();
+            // if ( puzzleElement != null && contactedGameObjects.Select((contacted) => contacted == puzzleElement).Count() == 0 )
+            // {
+            //     contactedGameObjects.Add(puzzleElement);
+            // }
+            
         }
     }
 
@@ -39,24 +45,31 @@ public class PuzzleElement : MonoBehaviour, IPointerClickHandler
     {
         if ( gameObject.name == collision2d.gameObject.name )
         {
-            this.contactedGameObject = null;
+            // var puzzleElement = collision2d.collider.gameObject.GetComponent<PuzzleElement>();
+            // if ( puzzleElement != null )
+            // {
+            //     contactedGameObjects.Remove(puzzleElement);
+            // }
         }
     }
 
     public void OnPointerClick(PointerEventData pointerEventData)
     {
-        if( this.contactedGameObject == null )
+        if( this.contactedGameObjects.Count == 0 )
         {
             return;
         }
-        else if ( this.contactedGameObject.name == gameObject.name )
+        else
         {
-            DestroyGameObjects(gameObject, this.contactedGameObject);
+            if ( remover.RegisterDeleteCandidates(this.contactedGameObjects) )
+            {
+                this.contactedGameObjects.ForEach((puzzleElement) => puzzleElement.OnPointerClick(pointerEventData));
+            }
+            else
+            {
+                remover.RemoveCandidates();
+            }
+            
         }
-    }
-
-    private void DestroyGameObjects(params GameObject[] gameObjects)
-    {
-        gameObjects.ToList().ForEach(Destroy);
     }
 }
--- Assets/Scripts/Puzzle/PuzzleElementGenerator.cs
+++ Assets/Scripts/Puzzle/PuzzleElementGenerator.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 using System.Linq;
+using System.Collections.Generic;
 
-public class PuzzleElementGenerator : MonoBehaviour
+public class PuzzleElementGenerator : MonoBehaviour, PuzzleElementRemover
 {
 
     private string[] elements = new string[] {
         "Puzzle/Prefabs/Pentagon",
-        "Puzzle/Prefabs/RoundedRectangle",
-        "Puzzle/Prefabs/Triangle"
+        // "Puzzle/Prefabs/RoundedRectangle",
+        // "Puzzle/Prefabs/Triangle"
     };
+
+    private List<PuzzleElement> removeCandidates = new List<PuzzleElement>();
 
     void OnEnable()
     {
@@ -34,16 +37,55 @@ public class PuzzleElementGenerator : MonoBehaviour
     private void OnGenerate(IEvent param)
     {
         GameObject prefab = Resources.Load<GameObject>(elements.Pick());
-        Instantiate<GameObject>(prefab, transform);
+        var garbage = Instantiate<GameObject>(prefab, transform);
+        garbage.GetComponent<PuzzleElement>().remover = this;
     }
 
     private void OnGarbageAcquire(IEvent param)
     {
         if( param is GarbageAcquireEvent garbageAcquireEvent )
         {
-            Debug.Log(garbageAcquireEvent.GetGarbageIdx());
-            GameObject prefab = Resources.Load<GameObject>(elements[garbageAcquireEvent.GetGarbageIdx()]);
-            Instantiate<GameObject>(prefab, transform);
+            //GameObject prefab = Resources.Load<GameObject>(elements[garbageAcquireEvent.GetGarbageIdx()]);
+            GameObject prefab = Resources.Load<GameObject>(elements[0]);
+            var garbage = Instantiate<GameObject>(prefab, transform);
+            garbage.GetComponent<PuzzleElement>().remover = this;
         }
+    }
+
+    public bool RegisterDeleteCandidates(List<PuzzleElement> contanctedGameObjects)
+    {
+        var changedCandidate = false;   
+        foreach( var contactedGameObject in contanctedGameObjects )
+        {
+            var foundedInCandidate = false;
+            foreach( var candidate in removeCandidates )
+            {
+                if ( candidate == contactedGameObject )
+                {
+                    foundedInCandidate = true;
+                    break;
+                }
+            }
+
+            if ( foundedInCandidate == false )
+            {
+                removeCandidates.Add(contactedGameObject);
+                changedCandidate = true;
+            }
+        }
+        return changedCandidate;
+    }
+    public void RemoveCandidates()
+    {
+        removeCandidates.ForEach(Destroy);
+        removeCandidates.Clear();
+    }
+
+    void RegisterDeleteCandidate(PuzzleElement contacted)
+    {
+
+    }
+    void UnregisterDeleteCandidate(PuzzleElement contacted)
+    {
     }
 }
